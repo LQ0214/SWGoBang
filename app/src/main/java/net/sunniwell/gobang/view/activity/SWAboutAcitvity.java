0232364aed6afa8aa9e81f03771420c341d400df
huangjinxing <huangjinxing@sunniwell.net>
2018-01-06 11:21:22 +0800

    modify：整合main界面

---
+++ app/src/main/java/net/sunniwell/gobang/view/activity/SWAboutAcitvity.java
@@ -0,0 +1,10 @@
+package net.sunniwell.gobang.view.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by Xing on 2018/1/6.
+ */
+
+public class SWAboutAcitvity extends Activity {
+}
--- app/src/main/java/net/sunniwell/gobang/view/activity/SWMainHomeActivity.java
+++ app/src/main/java/net/sunniwell/gobang/view/activity/SWMainHomeActivity.java
@@ -1,8 +1,11 @@
 package net.sunniwell.gobang.view.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import net.sunniwell.gobang.R;
@@ -15,10 +18,11 @@ import java.util.TimerTask;
  * Created by Xing on 2018/1/4.
  */
 
-public class SWMainHomeActivity extends Activity {
+public class SWMainHomeActivity extends Activity implements View.OnClickListener {
     private SWLogger log = SWLogger.getLogger(SWMainHomeActivity.class.getSimpleName());
     private Timer mTimer;
     private boolean mIsExit;
+    private Button mBtn_pvp, mBtn_pve, mBtn_setting, mBtn_about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,15 @@ public class SWMainHomeActivity extends Activity {
     private void init() {
         mTimer = new Timer();
         mIsExit = false;
+        mBtn_pvp = (Button) findViewById(R.id.main_home_pvp);
+        mBtn_pve = (Button) findViewById(R.id.main_home_pve);
+        mBtn_setting = (Button) findViewById(R.id.main_home_setting);
+        mBtn_about = (Button) findViewById(R.id.main_home_about);
+
+        mBtn_pvp.setOnClickListener(this);
+        mBtn_pve.setOnClickListener(this);
+        mBtn_setting.setOnClickListener(this);
+        mBtn_about.setOnClickListener(this);
     }
 
     @Override
@@ -47,7 +60,7 @@ public class SWMainHomeActivity extends Activity {
     }
 
     /**
-     * 双击退出
+     * 双击返回键退出
      */
     private void exit() {
         if (!mIsExit) {
@@ -75,5 +88,27 @@ public class SWMainHomeActivity extends Activity {
             mTimer = null;
         }
         super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_home_pvp:
+                Intent pvpIntent = new Intent(this, SWPvPActivity.class);
+                startActivity(pvpIntent);
+                break;
+            case R.id.main_home_pve:
+                Intent pveIntent = new Intent(this, SWPveActivity.class);
+                startActivity(pveIntent);
+                break;
+            case R.id.main_home_setting:
+                Intent setttingIntent = new Intent(this, SWSettingActivity.class);
+                startActivity(setttingIntent);
+                break;
+            case R.id.main_home_about:
+                Intent aboutIntent = new Intent(this, SWAboutAcitvity.class);
+                startActivity(aboutIntent);
+                break;
+        }
     }
 }
