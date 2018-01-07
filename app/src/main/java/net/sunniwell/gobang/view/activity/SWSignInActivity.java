400a0962e5ebc609e1bbcee0f1f17b8975ac2298
linhongbo <linhongbo@sunniwell.net>
2018-01-07 14:15:16 +0800

    modify:解决注册页面双击黑屏的问题。

--- app/src/main/java/net/sunniwell/gobang/view/activity/SWSignInActivity.java
+++ app/src/main/java/net/sunniwell/gobang/view/activity/SWSignInActivity.java
@@ -5,12 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
 import net.sunniwell.gobang.R;
 import net.sunniwell.gobang.utils.FragmentUtil;
 import net.sunniwell.gobang.view.fragment.SWRegisterFragment;
 import net.sunniwell.gobang.view.fragment.SWSignInFragment;
 import net.sunniwell.jar.log.SWLogger;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by lin on 2018/1/4.
@@ -23,9 +28,10 @@ public class SWSignInActivity extends FragmentActivity {
     public static final String SHARE_PREFERENCES_USER_NAME = "userName";
     public static final String SHARE_PREFERENCES_USER_PASSWORD = "userPassword";
     private static Activity mActivity;
+    private Timer mTimer;
 
     private FragmentManager mFragmentManager = getSupportFragmentManager();
-
+    private boolean mIsExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,9 +63,52 @@ public class SWSignInActivity extends FragmentActivity {
 
     private void initData() {
         mActivity = this;
+        mTimer = new Timer();
+        mIsExit = false;
         SWRegisterFragment registerFragment = new SWRegisterFragment();
         FragmentUtil.add(mFragmentManager, R.id.id_sign_in_root, registerFragment, SWRegisterFragment.class.getSimpleName());
         SWSignInFragment signInFragment = new SWSignInFragment();
         FragmentUtil.add(mFragmentManager, R.id.id_sign_in_root, signInFragment, SWSignInFragment.class.getSimpleName());
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        int action = event.getAction();
+        log.d("hjx    ==SWMainHomeActivity==>>>  action = " + "   keyCode = " + keyCode);
+        if (action == KeyEvent.ACTION_DOWN) {
+            exit();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * 双击返回键退出
+     */
+    private void exit() {
+        if (!mIsExit) {
+            mIsExit = true;
+            Toast.makeText(this, getResources().getString(R.string.main_home_exit), Toast.LENGTH_SHORT).show();
+            // 如果两秒内没连续点击2次，则认为不是退出行为
+            mTimer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    mIsExit = false;
+                }
+            }, 2000);
+
+        } else {
+            finish();
+            System.exit(0);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mTimer != null) {
+            mTimer.cancel();
+            mTimer = null;
+        }
+        super.onDestroy();
     }
 }
